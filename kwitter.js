1abf7895e83f95846d28d4ea2d669eb7326ecdc9
hudfgfk <116672298+hudfgfk@users.noreply.github.com>
2023-12-07 16:16:00 -0300

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCb7pKV5NvZ2OeN_qVAh6rSuG_qmfp3Vw0",
+  authDomain: "corrida-multi-5e00b.firebaseapp.com",
+  databaseURL: "https://corrida-multi-5e00b-default-rtdb.firebaseio.com",
+  projectId: "corrida-multi-5e00b",
+  storageBucket: "corrida-multi-5e00b.appspot.com",
+  messagingSenderId: "218197910579",
+  appId: "1:218197910579:web:5b6166e260b88878c5af46"
+};
+
+  firebase.initializeApp(firebaseConfig);
+  document.getElementById("userName").innerHTML = "Bem-vindo(a) " + userName + "!";
+ 
+  function login(){
+    userName = document.getElementById("userName").value;
+    localStorage.setItem("userName", userName);
+    window.location = 'kwitterRoom.html';
+  }
